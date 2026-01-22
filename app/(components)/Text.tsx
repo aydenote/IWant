ddcf5ccea715e03cdce980a702d37c4b38c1bb92
aydenote <dslgpgh@gmail.com>
2026-01-22 20:42:13 +0900

    feat: 공통 텍스트 컴포넌트 구현

---
+++ app/(components)/Text.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from 'react';
+import { TEXT_BOLD, TEXT_COLOR, TEXT_SIZE } from './constants/TextStyles';
+
+interface TextProps<T extends React.ElementType> {
+  textColor?: keyof typeof TEXT_COLOR;
+  textBold?: keyof typeof TEXT_BOLD;
+  textSize?: keyof typeof TEXT_SIZE;
+  as?: T;
+  className?: string;
+  children: ReactNode;
+}
+
+const Text = <T extends React.ElementType = 'p'>({
+  textColor,
+  textBold,
+  textSize,
+  as,
+  className = '',
+  children,
+  ...props
+}: TextProps<T>) => {
+  const Component = as ?? 'p';
+  const baseClass = [
+    textColor && TEXT_COLOR[textColor],
+    textBold && TEXT_BOLD[textBold],
+    textSize && TEXT_SIZE[textSize],
+    className,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  return (
+    <Component className={baseClass} {...props}>
+      {children}
+    </Component>
+  );
+};
+
+export default Text;
---
+++ app/(components)/constants/TextStyles.ts
@@ -0,0 +1,34 @@
+export const TEXT_COLOR = {
+  blue600: 'text-[var(--color-blue-600)]',
+  blue900: 'text-[var(--color-blue-900)]',
+
+  bluegray500: 'text-[var(--color-bluegray-500)]',
+
+  cyan500: 'text-[var(--color-cyan-500)]',
+  green500: 'text-[var(--color-green-500)]',
+  amber500: 'text-[var(--color-amber-500)]',
+
+  linear:
+    'bg-gradient-to-tr from-[#18ADCC] to-[#0D2F8F] bg-clip-text text-transparent',
+
+  white: 'text-white',
+  black: 'text-black',
+};
+
+export const TEXT_BOLD = {
+  sm: 'font-medium',
+  lg: 'font-bold',
+  xl: 'font-extrabold',
+  400: 'font-normal',
+  600: 'font-semibold',
+};
+
+export const TEXT_SIZE = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  md: 'text-base',
+  lg: 'text-lg',
+  '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+  '4xl': 'text-4xl',
+};
