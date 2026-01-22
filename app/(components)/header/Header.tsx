1707d445150afece4637cdb6e4b5373e3c1c2a5a
aydenote <dslgpgh@gmail.com>
2026-01-22 20:40:33 +0900

    design: 헤더 blur 스타일 추가

--- app/(components)/header/Header.tsx
+++ app/(components)/header/Header.tsx
@@ -6,7 +6,7 @@ import BriefcaseIcon from '../icons/BriefcaseIcon';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b">
+    <header className="sticky top-0 z-50 w-full border-b backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
         <Link className="flex items-center space-x-2" href="/">
           <BriefcaseIcon className="h-6 w-6 text-primary" />
