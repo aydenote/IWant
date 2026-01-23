8741614c87c2b0558c374731326a50dec4373b44
aydenote <dslgpgh@gmail.com>
2026-01-23 18:14:18 +0900

    feat: 공고 리스트 api 연결

---
+++ app/(apis)/job.ts
@@ -0,0 +1,13 @@
+import { JobListResponse } from '../types/apis';
+
+export const getJobList = async (): Promise<JobListResponse[]> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE}/navigation/v1/results?1765229525360=&job_group_id=518&job_ids=669&country=kr&job_sort=job.latest_order&years=0&locations=all`,
+    { cache: 'no-store' }
+  );
+
+  if (!res.ok) throw new Error('Failed to fetch job list');
+
+  const data = await res.json();
+  return data.data as JobListResponse[];
+};
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,33 +1,9 @@
 import Header from '../(components)/header/Header';
 import HomeClient from './HomeClient';
-import type { JobListResponse } from '../types/apis';
+import { getJobList } from '../(apis)/job';
 
-const Page = () => {
-  const jobList: JobListResponse[] = [
-    {
-      id: 334664,
-      company: {
-        id: 2541,
-        name: '휴먼컨설팅그룹',
-      },
-      address: {
-        location: '서울',
-        district: '서초구',
-      },
-      position: 'Product Engineer (신입 가능)',
-
-      annual_from: 0,
-      annual_to: 35,
-      skill_tags: [],
-      employment_type: 'regular',
-      title_img: {
-        origin:
-          'https://static.wanted.co.kr/images/company/2541/pcfxsoyjb69jhbdz__1080_790.jpg',
-        thumb:
-          'https://static.wanted.co.kr/images/company/2541/pcfxsoyjb69jhbdz__400_400.jpg',
-      },
-    },
-  ];
+export default async function Page() {
+  const jobList = await getJobList();
 
   return (
     <main>
@@ -35,6 +11,4 @@ const Page = () => {
       <HomeClient initialJobList={jobList} />
     </main>
   );
-};
-
-export default Page;
+}
