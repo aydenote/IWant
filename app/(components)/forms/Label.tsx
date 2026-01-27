f39d5d6be1636a1716e53a33fad60a6a2343c177
aydenote <dslgpgh@gmail.com>
2026-01-27 17:40:53 +0900

    feat: 라벨 컴포넌트 구현

---
+++ app/(components)/forms/Label.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+const Label = ({ className = '', ...props }: LabelProps) => {
+  const baseClasses =
+    'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70';
+
+  return <label className={`${baseClasses} ${className}`} {...props} />;
+};
+
+export default Label;
