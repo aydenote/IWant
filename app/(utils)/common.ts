3009d1a2b8c5ac2060df075925264838c3ba865d
aydenote <dslgpgh@gmail.com>
2026-01-29 18:56:57 +0900

    feat: 날짜 문자열 형태 변환 함수 구현

---
+++ app/(utils)/common.ts
@@ -0,0 +1,7 @@
+export const formatDate = (ms: number) => {
+  const date = new Date(ms);
+  const y = date.getFullYear();
+  const m = String(date.getMonth() + 1).padStart(2, '0');
+  const d = String(date.getDate()).padStart(2, '0');
+  return `${y}.${m}.${d}`;
+};
