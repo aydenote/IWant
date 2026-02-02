4dea0c774f1d7be29520811f287f808c2311cfc6
aydenote <dslgpgh@gmail.com>
2026-02-02 16:56:48 +0900

    feat: 프로필 관련 타입 추가

--- app/(types)/common.ts
+++ app/(types)/common.ts
@@ -2,3 +2,11 @@ export interface UploadedFileType {
   name: string;
   lastModified: number | string;
 }
+
+export type TechStackType = string[];
+export type ProfileNameType = string;
+
+export interface SaveProfileType {
+  techStack: TechStackType;
+  name: ProfileNameType;
+}
