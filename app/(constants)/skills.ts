87645ccb759f85241d887d2621fe1ab46463a312
aydenote <dslgpgh@gmail.com>
2026-02-14 00:33:58 +0900

    feat: 프론트엔드 기술 스택 정의

---
+++ app/(constants)/skills.ts
@@ -0,0 +1,78 @@
+export const FRONTEND_LANG = ['JavaScript', 'TypeScript', 'ES6', 'ESNext'];
+export const FRONTEND_FRAMEWORK = [
+  'React',
+  'Next.js',
+  'Vue',
+  'Nuxt',
+  'Angular',
+  'Svelte',
+  'SolidJS',
+  'NEXTJS',
+];
+export const FRONTEND_STATE = [
+  'Redux',
+  'Redux Toolkit',
+  'Recoil',
+  'Zustand',
+  'Jotai',
+  'TanStack Query',
+  'React Query',
+  'SWR',
+  'Apollo',
+  'GraphQL',
+];
+export const FRONTEND_STYLE = [
+  'CSS',
+  'SCSS',
+  'Sass',
+  'PostCSS',
+  'Tailwind',
+  'TailwindCSS',
+  'Styled Components',
+  'Emotion',
+  'CSS-in-JS',
+  'MUI',
+  'Material UI',
+  'Ant Design',
+  'Chakra UI',
+  'Shadcn',
+];
+export const FRONTEND_BUILD = [
+  'Webpack',
+  'Vite',
+  'Babel',
+  'ESBuild',
+  'Rollup',
+  'Turbopack',
+];
+export const FRONTEND_TEST = [
+  'Jest',
+  'Vitest',
+  'Cypress',
+  'Playwright',
+  'Testing Library',
+  'RTL',
+  'Storybook',
+  'ESLint',
+  'Prettier',
+];
+export const FRONTEND_RENDER = [
+  'SSR',
+  'SSG',
+  'ISR',
+  'CSR',
+  'Hydration',
+  'RSC',
+  'Suspense',
+];
+export const FRONTEND_INFRA = ['Vercel', 'Netlify', 'AWS', 'Docker', 'CI/CD'];
+export const FRONTEND_SKILL_KEYWORDS = [
+  ...FRONTEND_LANG,
+  ...FRONTEND_FRAMEWORK,
+  ...FRONTEND_STATE,
+  ...FRONTEND_STYLE,
+  ...FRONTEND_BUILD,
+  ...FRONTEND_TEST,
+  ...FRONTEND_RENDER,
+  ...FRONTEND_INFRA,
+];
