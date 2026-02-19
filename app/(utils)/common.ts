dc6176335a34c9b5573abfae048f52a5c55d1558
aydenote <dslgpgh@gmail.com>
2026-02-19 15:47:36 +0900

    refactor: 의미있는 변수명으로 수정

--- app/(utils)/common.ts
+++ app/(utils)/common.ts
@@ -1,7 +1,7 @@
 export const formatDate = (ms: number) => {
   const date = new Date(ms);
-  const y = date.getFullYear();
-  const m = String(date.getMonth() + 1).padStart(2, '0');
-  const d = String(date.getDate()).padStart(2, '0');
-  return `${y}.${m}.${d}`;
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}.${month}.${day}`;
 };
