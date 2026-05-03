6c5e590c310fa264d47cf3579dd8d1eddf6ae966
aydenote <dslgpgh@gmail.com>
2026-05-03 11:05:45 +0900

    feat: 페이지 타이틀 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { authOptions } from './api/auth/[...nextauth]/route';
 export const metadata = {
   title: {
     default: 'IWant',
-    template: '%s | IWant',
   },
   description:
     '개인이 입력한 기술스택으로 기여 가능한 오픈소스 레포를 찾도록 도와주는 서비스입니다',
